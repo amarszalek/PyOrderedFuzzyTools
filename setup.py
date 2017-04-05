c1c1d7a8b115a1a6acac41b5ad0ce6ba9a11e065
amarszalek <amarszalek@pk.edu.pl>
2017-04-05 11:27:41 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyorderedfuzzy',
+    version='0.0.1',
+    packages=['pyorderedfuzzy', 'pyorderedfuzzy.ofnumbers'],
+    url='',
+    license='',
+    author='amarszalek',
+    author_email='amarszalek@pk.edu.pl',
+    description='Python package for Ordered Fuzzy Numbers '
+)

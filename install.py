291ede5d2f8da125ae77a9be2b214724d2fc5f86
amarszalek <amarszalek@pk.edu.pl>
2017-04-10 16:50:50 +0200

    setup.py

--- install.py
+++ install.py
@@ -2,3 +2,4 @@ from subprocess import call
 
 call("swig -python -py3 pyorderedfuzzy/ofmodels/src/objective.i", shell=True)
 call("python pyorderedfuzzy/ofmodels/src/setup.py build_ext --build-lib='pyorderedfuzzy/ofmodels' --build-temp='pyorderedfuzzy/ofmodels/src'", shell=True)
+call("cp pyorderedfuzzy/ofmodels/src/objective.py pyorderedfuzzy/ofmodels/.")

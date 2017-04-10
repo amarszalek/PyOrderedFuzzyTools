8d39534dac2c9b746cb209d97d954fc66fa183a6
amarszalek <amarszalek@pk.edu.pl>
2017-04-10 14:39:49 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,6 @@ from distutils.command.build import build
 from setuptools.command.install import install
 import numpy
 
-
-class CustomBuild(build):
-    def run(self):
-        self.run_command('build_ext')
-        build.run(self)
-
-
-class CustomInstall(install):
-    def run(self):
-        self.run_command('build_ext')
-        self.do_egg_install()
-
 try:
     numpy_include = numpy.get_include()
 except AttributeError:
@@ -27,12 +15,11 @@ objective = Extension('pyorderedfuzzy.ofmodels._objective',
                       include_dirs=[numpy_include], swig_opts=['-py3', '-modern', '-I../include'])
 
 setup(
-    cmdclass={'build': CustomBuild, 'install': CustomInstall},
     name='pyorderedfuzzy',
     version='0.0.1',
     packages=['pyorderedfuzzy', 'pyorderedfuzzy.ofnumbers', 'pyorderedfuzzy.ofcandles',
               'pyorderedfuzzy.ofrandoms', 'pyorderedfuzzy.ofmodels'],
-    ext_modules=objective,
+    ext_modules=[objective],
     py_modules=['objective'],
     url='',
     license='',

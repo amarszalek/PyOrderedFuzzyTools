336370acc358039660d58dba5394ccc1b081686e
amarszalek <amarszalek@pk.edu.pl>
2017-04-10 18:31:02 +0200

    setup.py

--- pyorderedfuzzy/ofmodels/src/setup.py
+++ pyorderedfuzzy/ofmodels/src/setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup, Extension
 import numpy.distutils.misc_util
 
 setup(
-    ext_modules=[Extension("_objective", ["_objective.c", "objective.c", "utils.c"])],
+    ext_modules=[Extension("_objective", ["pyorderedfuzzy/ofmodels/src/_objective.c",
+                                          "pyorderedfuzzy/ofmodels/src/objective.c",
+                                          "pyorderedfuzzy/ofmodels/src/utils.c"])],
     include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
 )
 

47c3b45d43aa7a3aff07c84d13e2b17356206773
amarszalek <amarszalek@pk.edu.pl>
2017-04-10 16:32:36 +0200

    setup.py

--- pyorderedfuzzy/ofmodels/src/setup.py
+++ pyorderedfuzzy/ofmodels/src/setup.py
@@ -6,8 +6,11 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
-objective = Extension('_objective', sources=['objective_wrap.c', 'objective.c', 'utils.c'],
-                       include_dirs=[numpy_include],)
+objective = Extension('_objective',
+                      sources=['pyorderedfuzzy/ofmodels/src/objective_wrap.c',
+                               'pyorderedfuzzy/ofmodels/src/objective.c',
+                               'pyorderedfuzzy/ofmodels/src/utils.c'],
+                      include_dirs=[numpy_include],)
 
 setup(name='objective',
       description="""c extensions""",

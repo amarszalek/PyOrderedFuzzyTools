cff91b0085e77d9b6ab9c2b2e0b35bb86a2ceb01
amarszalek <amarszalek@pk.edu.pl>
2017-04-10 15:06:23 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from distutils.core import setup, Extension
-from distutils.command.build import build
-from setuptools.command.install import install
 import numpy
 
 try:
@@ -8,18 +6,21 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
-objective = Extension('pyorderedfuzzy.ofmodels._objective',
-                      sources=['pyorderedfuzzy/ofmodels/src/objective.c',
-                               'pyorderedfuzzy/ofmodels/src/utils.c',
-                               'pyorderedfuzzy/ofmodels/src/objective.i'],
-                      include_dirs=[numpy_include], swig_opts=['-py3', '-modern', '-I../include'])
+objective = Extension('_objective',
+                      sources=['pyorderedfuzzy/ofmodels/src/objective.i',
+                               'pyorderedfuzzy/ofmodels/src/objective.c',
+                               'pyorderedfuzzy/ofmodels/src/utils.c'],
+                      include_dirs=[numpy_include],
+                      swig_opts=['-py3', '-modern', '-I../include'])
 
 setup(
     name='pyorderedfuzzy',
     version='0.0.1',
     packages=['pyorderedfuzzy', 'pyorderedfuzzy.ofnumbers', 'pyorderedfuzzy.ofcandles',
               'pyorderedfuzzy.ofrandoms', 'pyorderedfuzzy.ofmodels'],
-    ext_modules=[objective],
+
+    ext_modules=[objective, ],
+    py_modules=['objective', ],
     url='',
     license='',
     author='amarszalek',

9fcf5ae76c596f0a60ceb719fc6f308e590ef9d3
amarszalek <amarszalek@pk.edu.pl>
2017-04-12 10:19:10 +0200

    cmle

--- pyorderedfuzzy/ofmodels/ofgarch.py
+++ pyorderedfuzzy/ofmodels/ofgarch.py
@@ -11,22 +11,19 @@ __author__ = "amarszalek"
 
 
 class OFGARCH(object):
-    def __init__(self, order_p=1, order_q=1, intercept=True):
+    def __init__(self, order_p=1, order_q=1):
         super(OFGARCH, self).__init__()
-        self.intercept = intercept
         self.order_p = order_p
         self.order_q = order_q
         self.coef = OFSeries([])
         self.residuals = OFSeries([])
+        self.llf
 
-    def fit(self, ofseries, order_p, order_q, intercept=True, solver='L-BFGS-B', options={}):
+    def fit(self, ofseries, order_p, order_q, solver='L-BFGS-B', options={}):
         dim = ofseries[0].branch_f.dim
         self.order_p = order_p
         self.order_q = order_q
-        self.intercept = intercept
         n_coef = order_p + order_q + 1
-        if self.intercept:
-            n_coef += 1
 
         # initial coef
         ran = np.random.random(n_coef)
@@ -37,7 +34,7 @@ class OFGARCH(object):
                 options = {'disp': None, 'gtol': 1.0e-12, 'eps': 1e-08, 'maxiter': 1000, 'ftol': 2.22e-09}
             p0 = ofns2array(coef)
             ofns = ofns2array(ofseries)
-            args = (order_p, order_q, n_coef, dim, ofns, intercept)
+            args = (order_p, order_q, n_coef, dim, ofns)
             res = minimize(fun_obj_qml, p0, args=args, method='L-BFGS-B', jac=True, options=options)
             coef = array2ofns(res.x, n_coef, dim)
             self.coef = OFSeries(coef)
@@ -55,57 +52,41 @@ class OFGARCH(object):
 
 
 def compute_ht(coef, past_x, past_h):
-    ht = coef[0]
+    ht = np.exp(coef[0])
     for p in range(1, len(past_x)+1):
-        ht = ht + coef[p] * past_x[-p]
+        ht = ht + coef[p] * coef[p] * past_x[-p]
     p = len(past_x) + 1
     for q in range(1, len(past_h)+1):
-        ht = ht + coef[p+q] * past_x[-q]
+        ht = ht + coef[p+q-1] * coef[p+q-1] * past_x[-q]
     return ht
 
 
-def fun_obj_qml(p, order_p, order_q, n_coef, dim, ofns, intercept):
+def fun_obj_qml(p, order_p, order_q, n_coef, dim, ofns):
     e = 0.0
+    grad = np.zeros(len(p))
     n_cans = int(len(ofns) / (2 * dim))
     can = ofns.reshape((n_cans, 2 * dim))
     coef = p.reshape((n_coef, 2 * dim))
-    grad = np.zeros(len(p))
-    if intercept:
-        past_x = np.zeros((order_p, 2 * dim))
-        beta = 1.0
-        for i in range(order_q):
-            beta = beta - coef[order_p+2+i]
-        past_h = np.array([coef[1]/beta for _ in range(order_q)])
-        for i in range(n_cans):
-            r = can[i] - coef[0]
-            ht = compute_ht(coef[1:], past_x, past_h)
-            past_x[:-1] = past_x[1:]
-            past_x[-1] = r*r
-            past_h[:-1] = past_h[1:]
-            past_h[-1] = ht
-            e += np.sum((0.5*r*r)/ht + np.log(ht))
-            grad[:2 * dim] -= r/ht
-            grad[2*dim:4*dim] += 0.5*(1/ht - (r*r)/(ht*ht))
-            for j in range(2, order_p+2):
-                grad[2 * dim * j:2 * dim * (j + 1)] += 0.5*(1/ht - (r*r)/(ht*ht))*past_x[-2]*past_x[-2]
-            for j in range(order_p+2, order_p+order_q+2):
-                grad[2 * dim * j:2 * dim * (j + 1)] +=  0.5*(1/ht - (r*r)/(ht*ht))*past_h[-2]*past_h[-2]
-    else:
-        past_x = np.zeros((order_p, 2 * dim))
-        beta = 1.0
-        for i in range(order_q):
-            beta = beta - coef[order_p + 1 + i]
-        past_h = np.array([coef[0] / beta for _ in range(order_q)])
-        for i in range(n_coef, n_cans):
-            r = can[i]
+
+    past_x = np.zeros((order_p, 2 * dim))
+    beta = coef[order_p+1]*coef[order_p+1]
+    for i in range(1, order_q):
+        beta = beta + coef[order_p+1+i]*coef[order_p+1+i]
+    past_h = np.array([np.exp(coef[0])/(1.0-beta) for _ in range(order_q)])
+
+    for i in range(n_cans):
+            r = can[i]*can[i]
             ht = compute_ht(coef, past_x, past_h)
+            e += np.sum(0.5*(r/ht + np.log(ht)))
+            grad[:2 * dim] += 0.5*(1.0/ht - r/(ht*ht))*np.exp(coef[0])
+            for j in range(1, order_p+1):
+                grad[2 * dim * j:2 * dim * (j + 1)] += (1.0/ht - r/(ht*ht))*past_x[-1]*coef[j]
+            for j in range(order_p+1, order_p+order_q+1):
+                grad[2 * dim * j:2 * dim * (j + 1)] += (1.0/ht - r/(ht*ht))*past_h[-1]*coef[j]
             past_x[:-1] = past_x[1:]
-            past_x[-1] = can[i]
+            past_x[-1] = r
             past_h[:-1] = past_h[1:]
             past_h[-1] = ht
-            e += np.sum((0.5 * r * r) / ht + np.log(ht))
-            #for j in range(n_coef):
-            #    grad[2 * dim * j:2 * dim * (j + 1)] -= 2 * r * can[i - j - 1]
     return e, grad
 
 
